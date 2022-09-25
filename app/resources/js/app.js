3545712524a64f33de7a9ab1441de37bff5c71ac
nishinaga-kento <mbypaceus.928.2k8n@gmail.com>
2022-09-26 00:00:09 +0900

    test

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,9 +1,1 @@
 require('./bootstrap');
-
-import { createApp } from 'vue'
-import TestVue from './components/TestVue.vue';
-import '../css/app.css';
-
-const app = createApp({})
-app.component('test-vue', TestVue);
-app.mount('#app')
